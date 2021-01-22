e07d80048c2329c3f8f98524906d4f7d5d562958
n0tybynature <74768895+n0tybynature@users.noreply.github.com>
2021-01-22 09:27:22 -0800

    testing

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
-// Write your Character component here
+import React, {useEffect, useState} from "react"
+import axios from "axios"
+import styled from "styled-components"
+
+
+
+export default function Character (props){
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData= () => {
+            axios.get('https://rickandmortyapi.com/api/character')
+            .get()
+        }
+    })
+
+
+}
